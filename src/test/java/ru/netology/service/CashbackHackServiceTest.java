3ec3528e690f558aa8621425f9693e56336e7baf
Votgosh <99820455+Votgosh@users.noreply.github.com>
2022-06-05 21:53:47 +0400

    testng

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,30 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void buyMax() {
+        int amount = 900;
+        int expected = 100;
+
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+
+        assertEquals(actual,expected);
+    }
+
+    @Test
+    public void buyMin() {
+        int amount = 1;
+        int expected = 999;
+
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+
+        assertEquals(actual,expected);
+    }
+}
